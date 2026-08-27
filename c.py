22e23a2eba17f73349f03f18a539020f3dbfee9d
barbod <barbod@fgdhgmail.com>
2026-08-27 16:07:51 +0800

    barbod_shahrad

---
+++ c.py
@@ -0,0 +1,24 @@
+age = 13
+print(age)
+print(type(age))
+
+price = 145.5
+print(price)
+print(type(price))
+
+name = "barbod_shahrad"
+print(name)
+print(type(name))
+
+is_student = True
+print(is_student)
+print(type(is_student))
+
+name = "barbod_shahrad"
+age = 13
+height = 1.70
+is_student = True
+print(name)
+print(age)
+print(height)
+print(is_student)
