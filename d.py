bc8abb45986ccfaf7be0c756ed49fe4950d12b33
barbod <barbod@fgdhgmail.com>
2026-08-31 13:21:19 +0330

    barbod

---
+++ d.py
@@ -0,0 +1,20 @@
+class person:
+    name = 'barbod'
+    age = 13
+
+    def say_hello(self):
+        print("hello")
+
+
+
+p1 = person()
+p1.name = 'barbod'
+print(p1.name)
+p1.say_hello()
+
+p2 = person()
+p2.name = 'mahbod' 
+p2.say_hello()
+print(p2.name)
+
+
