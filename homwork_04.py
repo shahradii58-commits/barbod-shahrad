baf02332e8a35f8b4e96ee8827201d2f55ad9778
barbod <barbod@fgdhgmail.com>
2026-09-01 11:44:32 +0330

    barbod

---
+++ homwork_04.py
@@ -0,0 +1,11 @@
+#1:
+number = int(input("enter a number: "))
+
+if number > 15:
+    print("you are allowed to enter")
+
+#2:
+text = input("enter a text: ")
+
+if len(text) > 40:
+    print("the text is more than 40 characters")
