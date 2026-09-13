0144b57f40d02c8ed5dd8932e1177c86b81b1878
barbod <barbod@fgdhgmail.com>
2026-09-13 12:58:00 +0330

    barbod

--- tamrin_2.py
+++
@@ -1,8 +0,0 @@
-from kivy.app import App
-from kivy.uix.label import Label
-
-class myapp(App):
-    def build(self):
-        label = Label(text="hello barbod and shahrad")
-        return label
-myapp().run()
